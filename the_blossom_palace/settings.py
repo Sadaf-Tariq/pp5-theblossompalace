8b32e912bd17c1ab4f28baca39d807f13ba7bcf7
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2024-02-27 12:10:01 +0000

    Add elephant sql and setup

--- the_blossom_palace/settings.py
+++ the_blossom_palace/settings.py
@@ -110,12 +110,18 @@ WSGI_APPLICATION = 'the_blossom_palace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
 
 
 # Password validation
